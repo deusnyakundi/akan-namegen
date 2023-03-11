8480972398c51ee5d48ae462ace73f0427fd8573
Deus Nyakundi <nyakundideus1@gmail.com>
2023-03-11 10:01:52 +0300

    JS troubleshoot

---
+++ scripts/script.js
@@ -0,0 +1,69 @@
+//Add Javascript here 
+
+//Functions to validate data
+function validateDay(){
+    let day = parseInt(document.getElementById('day').value)
+    let dayError;
+
+    if(isNaN(day) || day<=0 || day>31){
+        dayError = "Day can only be from 1 to 31"
+        document.getElementById('day-error').innerHTML = dayError
+        return dayError
+    }
+};
+
+function validateMonth(){
+    let month = parseInt(document.getElementById('month').value)
+    let monthError;
+
+    if(isNaN(month) || month<=0 || month>12){
+        monthError = "There are only 12 months in a year."
+        document.getElementById('month-error').innerHTML = monthError
+    }
+};
+
+function validateYear(){
+    let year = document.getElementById('year').value
+    let yearError
+    
+    if(year.length > 4 || isNaN(year)){
+        yearError = "Invalid input."
+        document.getElementById('year-error').innerHTML = yearError
+    }
+};
+
+
+//Function to get the day of the week from dates
+function dayOfTheWeek(){
+    validateDay()
+    validateMonth()
+    validateYear()
+    var DD = document.getElementById('day').value
+    var MM = document.getElementById('month').value - 1
+    var year = document.getElementById('year').value
+
+    var dateOfBirth = new Date(year, MM, DD)
+    let day = dateOfBirth.getDay()
+    console.log(day)
+    return day;
+}
+
+//Function to get the gender
+
+function userGender(){
+    let male = document.getElementById('male').checked
+    let female = document.getElementById('female').checked
+    let genderError
+    if( male === true){
+        var gender = "male"
+        return gender;
+    }else if(female === true){
+        var gender = "female"
+        return gender;
+    }else{
+        genderError="Please select a gender"
+        document.getElementById('gender-error').innerHTML = genderError
+    }
+}
+
+
