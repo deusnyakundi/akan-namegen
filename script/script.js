d6fa7160acbe2aab99df3384114bb12c22b6efe4
Deus Nyakundi <nyakundideus1@gmail.com>
2023-03-11 09:39:12 +0300

    Function to validate date

--- script/script.js
+++ script/script.js
@@ -1,1 +1,13 @@
 //Add Javascript here 
+
+//Functions to validate data
+function validateDay(){
+    let day = parseInt(document.getElementById('day').value)
+    let dayError;
+
+    if(isNaN(day) || day<=0 || day>31){
+        dayError = "Day can only be from 1 to 31"
+        document.getElementById('day-error').innerHTML = dayError
+        return dayError
+    }
+};
