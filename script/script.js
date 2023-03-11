2725aa256a92ab7df526dcf4dcffd765986098b6
Deus Nyakundi <nyakundideus1@gmail.com>
2023-03-11 09:39:50 +0300

    Function to validate Month and Year

--- script/script.js
+++ script/script.js
@@ -11,3 +11,24 @@ function validateDay(){
         return dayError
     }
 };
+
+function validateMonth(){
+    let month = parseInt(document.getElementById('month').value)
+    let monthError;
+
+    if(isNaN(month) || month<=0 || month>12){
+        monthError = "There are only 12 months in a year."
+        document.getElementById('month-error').innerHTML = monthError
+    }
+};
+
+function validateYear(){
+    let year = document.getElementById('year').value
+    let yearError
+    
+    if(year.length > 4 || isNaN(year)){
+        yearError = "Invalid input."
+        document.getElementById('year-error').innerHTML = yearError
+    }
+};
+
