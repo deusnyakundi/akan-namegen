fbd3f61e5f300a02118ed913ce6a073ea4e67a4d
Deus Nyakundi <deusnyakundi@Alchemist-2.local>
2023-03-12 22:53:29 +0300

    Validate date and month

--- scripts/script.js
+++ scripts/script.js
@@ -15,4 +15,33 @@ function getAkanName () {
     let myGenderValue = getGender();
     console.log(myGenderValue);
 
+
 }
+
+// validation functions
+function dayValidator () {
+    if (monthOfBirth === 2 && Number(yearOfBirth)%4 === 0) {
+      if (dayOfBirth > 28 || dayOfBirth < 1) {
+        return false;
+      } else if (monthOfBirth === 2 && dayOfBirth > 29) {
+        return false;
+      } else if (monthOfBirth === 2 && dayOfBirth < 1) {
+        return false;
+      } else {
+        return true;
+      }
+    } else if (dayOfBirth < 1 || dayOfBirth > 31){
+      return false;
+    } else {
+      return true;
+    }
+  }
+  
+
+function monthValidator () {
+    if (monthOfBirth < 1 || monthOfBirth > 12) {
+      return false;
+    } else {
+      return true;
+    }
+  }
